a8c04ebfbad0085fada677e56c2f6548d68eb6c1
Johnathan Umana <60320817+Johnathan238@users.noreply.github.com>
2022-07-26 17:54:18 -0700

    [AN-02] Add Todo & Button Todo (#3)
    * Successfully added toggleDone method & deleteTodo method
    
    * Succesfully added addTodo method & button Add Todo

--- angular-list/src/app/components/todos/todos.component.ts
+++ angular-list/src/app/components/todos/todos.component.ts
@@ -10,7 +10,9 @@ import { Todo } from '../../models/Todo'
 // ^ Data Binding - Eject to the DOM ^ //
 export class TodosComponent implements OnInit {
 
-  todos:Todo[]
+  todos: Todo[]
+
+  inputTodo:string = ""
 
   constructor() { }
 
@@ -25,5 +27,26 @@ export class TodosComponent implements OnInit {
         completed: false
       },
     ]
+  }
+
+  toggleDone(id:number) {
+    this.todos.map((v, i) => {
+      if (i === id) v.completed = !v.completed
+
+      return v
+    })
+  }
+
+  deleteTodo(id:number) {
+    this.todos = this.todos.filter((v, i) => i !== id)
+  }
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed:false
+    })
+
+    this.inputTodo = ""
   }
 }
