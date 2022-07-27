fc670300887b89ce35ac788290ad006d48430a66
Johnathan Umana <60320817+Johnathan238@users.noreply.github.com>
2022-07-26 17:04:13 -0700

    [AN-01] (#2)
    * Was getting an error & found solution on Grepper
    
    * Succesfully got todo list on DOM

--- angular-list/src/app/components/todos/todos.component.ts
+++ angular-list/src/app/components/todos/todos.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo'
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
+
+// ^ Data Binding - Eject to the DOM ^ //
 export class TodosComponent implements OnInit {
+
+  todos:Todo[]
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First todo',
+        completed: false
+      },
+      {
+        content: 'Second todo',
+        completed: false
+      },
+    ]
   }
-
 }
